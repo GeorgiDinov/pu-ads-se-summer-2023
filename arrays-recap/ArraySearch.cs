e03019a589332375fa14ffdd3d8c066c4f8cd2de
Georgi Dinov <georgidinov1@abv.bg>
2023-02-12 21:45:58 +0200

    Added array declarations visualization, linear and binary search methods

---
+++ arrays-recap/ArraySearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_recap
+{
+    internal class ArraySearch
+    {
+
+
+        //Linear search
+        private static int LinearSearch(int[] arrayToSearchIn, int numberToSearchFor)
+        {
+            for (int i = 0; i < arrayToSearchIn.Length; i++)
+            {
+                if (arrayToSearchIn[i] == numberToSearchFor)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }// end of method LinearSearch
+
+
+        //Binary search
+        //*** remeber, works only on sorted array/collection
+        private static int BinarySearch(int[] arrayToSearchIn, int numberToSearchFor)
+        {
+            int first = 0;
+            int last = arrayToSearchIn.Length - 1;
+            while (first <= last)
+            {
+                int mid = (first + last) / 2;
+                if (arrayToSearchIn[mid] == numberToSearchFor)
+                {
+                    return mid;
+                }
+
+                if (arrayToSearchIn[mid] < numberToSearchFor)
+                {
+                    first = mid + 1;
+                }
+                else
+                {
+                    last = mid - 1;
+                }
+            }
+            return -1;
+        }// end of method BinarySearch
+
+
+    }// end of class ArraySearch
+}
---
+++ arrays-recap/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_recap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArrayDeclarations();
+        }
+
+        private static void PrintArrayDeclarations()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            PrintSingleDimentionArray(array, "Array declaration 'int[] array = { 1, 2, 3, 4, 5 };'   = ");
+            array = new int[] { 1, 2 };
+            PrintSingleDimentionArray(array, "Array declaration 'int[] array = new int[] { 1, 2 };'  = ");
+            array = new int[5];
+            PrintSingleDimentionArray(array, "Array declaration 'int[] array = new int[5];'          = ");
+        }
+
+        private static void PrintSingleDimentionArray(int[] array, string prefix)
+        {
+            string arrString = GetArrayAsString(array);
+            string message = prefix + arrString;
+            Console.WriteLine(message);
+        }
+
+        private static string GetArrayAsString(int[] array)
+        {
+            string result = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += (i < array.Length - 1) ? array[i] + ", " : array[i] + "";
+            }
+            return "[" + result + "]";
+        }
+    }
+}
