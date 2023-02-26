59a1bf36a639ce4d728a5d004b31f761fafc6806
Georgi Dinov <georgidinov1@abv.bg>
2023-02-26 12:11:33 +0200

    Added project for searching/ graph algorithms and basic bfs crawler example

---
+++ search-algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search_algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startingUrl = "http://fmi-plovdiv.org";
+            new BFSBasedSimpleWebCrawler().crawlTheWebFrom(startingUrl);
+        }
+    }
+}
